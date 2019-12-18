d7d7c3feebcce61dbce02ac2b73366f9af475cf9
8989Rahul <daghalerahul0@gmail.com>
2019-12-18 11:57:40 +0530

    added some navigation

--- components/AppNavigator.js
+++ components/AppNavigator.js
@@ -1,10 +1,46 @@
-import {createStackNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import CreateNotification from './CreateNotification/CreateNotification';
 import AddRecipient from './CreateNotification/CreateNotificationHeader/SendMessage/AddRecipient';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {Animated, Easing} from 'react-native';
+const AppNavigator = createStackNavigator(
+  {
+    CreateNotification: {
+      screen: CreateNotification,
+    },
+    AddRecipient: {screen: AddRecipient},
+  },
+  {
+    initialRouteName: 'CreateNotification',
+    headerMode: 'none',
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing,
+      },
+      screenInterpolator: sceneProps => {
+        const {layout, position, scene} = sceneProps;
+        const {index} = scene;
 
-const AppNavigator = createStackNavigator({
-  CreateNotification: {screen: CreateNotification},
-  AddRecipient: {screen: AddRecipient},
-});
+        const height = layout.initHeight;
+        const translateY = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [height, 0, 0],
+        });
 
-export default AppNavigator;
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        });
+
+        return {opacity, transform: [{translateY}]};
+      },
+    }),
+  },
+);
+export default createAppContainer(
+  createSwitchNavigator({
+    AppNavigator: AppNavigator,
+  }),
+);
