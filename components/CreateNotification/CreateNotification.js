b9e61770f1423b65f48674240fe7dc90f66b9a9e
8989Rahul <daghalerahul0@gmail.com>
2020-01-11 12:03:56 +0530

    added some files

--- components/CreateNotification/CreateNotification.js
+++ components/CreateNotification/CreateNotification.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import CreateNotificationHeader from './CreateNotificationHeader/CreateNotificationHeader';
-import Subject from './Subject/Subject';
+import Description from './Description/Description';
 import SetDateAndTime from './SetDateAndTime/SetDateAndTime';
+import SendMessage from './CreateNotificationHeader/SendMessage/SendMessage';
+import Save from './Save';
+import {ScrollView} from 'react-native';
 
-const CreateNotification = () => {
+const CreateNotification = props => {
+  const name = props.navigation.getParam('name')
+    ? props.navigation.getParam('name')
+    : '';
+  const phone = props.navigation.getParam('phone')
+    ? props.navigation.getParam('phone')
+    : '';
   return (
     <Wrapper>
       <CreateNotificationHeader />
-      <Container>
-        <Subject />
-        <SetDateAndTime />
-      </Container>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Container>
+          <Description placeholder="Description" />
+          <SetDateAndTime />
+          <SendMessage
+            navigation={props.navigation}
+            name={name}
+            phone={phone}
+          />
+        </Container>
+      </ScrollView>
+      <Save />
     </Wrapper>
   );
 };
@@ -19,9 +36,10 @@ const CreateNotification = () => {
 export default CreateNotification;
 
 const Wrapper = styled.View`
+  flex: 1;
   background-color: #f5f6fb;
 `;
 
 const Container = styled.View`
-  padding-horizontal: 15px;
+  padding-horizontal: 10px;
 `;
